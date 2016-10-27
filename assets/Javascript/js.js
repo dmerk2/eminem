8252cad34a4cd3f166cf3326ffe49c82b7948b68
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-10-27 16:41:20 -0400

    added API from ombd.com movies

--- assets/Javascript/js.js
+++ assets/Javascript/js.js
@@ -1,3 +1,24 @@
+	// This .on("click") function will trigger the AJAX Call
+	$('#findMovie').on('click', function(){
+
+		// Here we grab the text from the input box 
+		var movie = $('#movie-input').val();
+
+		// Here we assemble our URL 
+		var queryURLomdb = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json";
+
+		$.ajax({url: queryURLomdb, method: "GET"})
+		.done(function(response){
+		  console.log(response);
+		  $('#movieView').html(JSON.stringify(response));
+		 
+		});
+		return false;
+	})
+
+
+
+
 $('button').on('click', function() {
 	var p = $(this).data('person');
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -7,6 +28,7 @@ $('button').on('click', function() {
 	  	method: 'GET'
 		})
 		.done(function(response) {
+			console.log(response)
 	var results = response.data;
 
 for (var i = 0; i < results.length; i++) {
