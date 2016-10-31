d53308edfb918405f86c6479902b6bbdde752c08
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-10-31 17:11:11 -0400

    fixing giphy search Jquery

--- assets/Javascript/js.js
+++ assets/Javascript/js.js
@@ -1,27 +1,69 @@
-	// This .on("click") function will trigger the AJAX Call
-	$('#findMovie').on('click', function(){
+//Search Movie Info	
+// This .on("click") function will trigger the AJAX Call
+$('#findMovie').on('click', function(){
 
-		// Here we grab the text from the input box 
-		var movie = $('#movie-input').val();
+	// Here we grab the text from the input box 
+	var movie = $('#movie-input').val();
 
-		// Here we assemble our URL 
-		var queryURLomdb = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json";
+	// Here we assemble our URL 
+	var queryURLomdb = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json";
 
-		$.ajax({url: queryURLomdb, method: "GET"})
-		.done(function(response){
-		  console.log(response);
-		  $('#movieView').html(JSON.stringify(response));
-		 
-		});
+	$.ajax({url: queryURLomdb, method: "GET"})
+	.done(function(response){
+	  console.log(response);
+	  $('#movieView').html(JSON.stringify(response));
+	 
+	});
+	return false;
+});
+
+
+//Search Giphys
+  
+var inputBox = $('#giphy-input')
+
+$('#findGiphy').on('click', function(){
+	var userChoice = $('#giphy-input').val();
+	renderImage(userChoice);
+	console.log(choiceArray);
+});
+
+function renderImage (userChoice) {
+		var image = $('<img>');
+		image.text(userChoice);
+		$('#giphyButton').append(images);
+		
 		return false;
-	})
+		var giphy = $(this).data('giphy');
+
+	// Here we assemble our URL 
+	var queryURLgiphy = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	$.ajax({url: queryURLgiphy, method: "GET"})
+		.done(function(response){
+	  	console.log(response);
+			var results = response.data;
+
+			for (var i = 0; i < response.data.length; i++) {
+				var giphyDiv = response.data[i].images.fixed_height.url;
+				var giphyImage = $('<img>');
+				giphyImage.attr('src', giphyDiv);
+				giphyImage.attr('alt', 'giphyImage');
+				$('#giphyView').prepend(giphyImage);
+		};
+	});   
+};
 
 
 
+	
+	
 
+
+//Eminem Giphys
 $('button').on('click', function() {
 	var p = $(this).data('person');
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=20";
 
 		$.ajax({
 	 		url: queryURL,
@@ -29,20 +71,19 @@ $('button').on('click', function() {
 		})
 		.done(function(response) {
 			console.log(response)
-	var results = response.data;
+			var results = response.data;
 
-for (var i = 0; i < results.length; i++) {
-    var gifDiv = $('<div class="item">')
+			for (var i = 0; i < response.data.length; i++) {
+				var imageUrl = response.data[i].images.fixed_height.url;
 
-    var personImage = $('<img>');
-    personImage.attr('src', results[i].images.fixed_height.url);
+    		var personImage = $('<img>');
+		    personImage.attr('src', imageUrl);
+		    personImage.attr('alt', 'personImage');
 
-    gifDiv.append(personImage)
-    console.log(p)
-
-    $('#gifsAppearHere').prepend(gifDiv);
-
-  	}
-	});
+		    $("#gifsAppearHere").prepend(personImage);
+		    // console.log(p);
+		    // console.log(results)
+		    // console.log(personImage)
+			};  	
+		});
 });
-
